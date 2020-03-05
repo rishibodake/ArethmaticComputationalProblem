5635e77de9fe6d435e745d2b051b68b86a77af22
rishibodake <rishibodake@gmail.com>
2020-03-05 19:01:59 +0530

    Added:takingInputFeature

---
+++ arethmaticComputationalProblem.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+#Discreiption:This problem computes different arithmetic expressions and Sorts the results
+#Author:Hrishikesh M Bodake
+#Since:March 5 2020 /Thursday
+
+function getInputs(){
+	read -p "Enter first Number: " firstNumber
+	read -p "Enter second Number: " secondNumber
+	read -p "Enter third Number: " thirdNumber
+}
+getInputs
