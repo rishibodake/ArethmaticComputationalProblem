f9501dd3c952deee91668917f1a4e5fe857de71d
rishibodake <rishibodake@gmail.com>
2020-03-06 10:35:23 +0530

    Added:StoringResultInDictionaryFeature

--- arethmaticComputationalProblem.sh
+++ arethmaticComputationalProblem.sh
@@ -3,32 +3,41 @@
 #Discreiption:This problem computes different arithmetic expressions and Sorts the results
 #Author:Hrishikesh M Bodake
 #Since:March 5 2020 /Thursday
+declare -A resultStore
 
 function getInputs(){
 	read -p "Enter first Number: " firstNumber
 	read -p "Enter second Number: " secondNumber
 	read -p "Enter third Number: " thirdNumber
+	firstExpression
 }
 function firstExpression(){
 	#Exrpession is a+b*c
-	getInputs
 	firstResult=$((($firstNumber+$secondNumber*$thirdNumber)))
+	resultStore[firstResult]=$firstResult
+	secondExpression
 }
 function secondExpression(){
 	#Expression is a*b+c
-	getInputs
 	secondResult=$((($firstNumber*$secondNumber+$thirdNumber)))
+	resultStore[secondResult]=$secondResult
+	thirdExpression
+
 }
 function thirdExpression(){
 	#Expression is c+a/b
-	getInputs
 	thirdResult=`expr "scale=2;$thirdNumber+$firstNumber/$secondNumber" | bc -l`
+	resultStore[thirdResult]=$thirdResult
+	fourthExpression
+
 }
 function fourthExpression(){
 	#Expression is a%b+c
-	getInputs
 	fourthResult=`expr "scale=2;$firstNumber%$secondNumber+$thirdNumber" | bc -l`
+	resultStore[fourthResult]=$fourthResult
 }
-fourthExpression
+getInputs
+echo ${resultStore[@]}
+echo ${!resultStore[@]}
 
 
